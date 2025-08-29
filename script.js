8b0afff035283bb8e2ba4b473f552232eedacc8c
Abhishekkumarwith <abhishekk39861@gmail.com>
2025-08-29 12:34:35 +0530

    added js file for counter app

---
+++ script.js
@@ -0,0 +1,39 @@
+(function(){
+const countDisplay = document.getElementById("number-counter");
+const incrementButton = document.querySelector("#increment button:nth-child(2)");
+const decrementButton = document.querySelector("#increment button:nth-child(1)");
+const resetButton = document.getElementById("reset-btn");
+const inputField = document.getElementById("myInput");
+let plus=parseInt(inputField.value)
+let minus=parseInt(inputField.value)
+incrementButton.addEventListener("click", () => {
+  const countValue =parseInt(countDisplay.innerText)
+  countDisplay.innerText=countValue + plus
+  console.log('Plus Operator');
+  
+});
+
+decrementButton.addEventListener("click", () => {
+const countValue=parseInt(countDisplay.innerText)
+  countDisplay.innerText = countValue-minus
+  console.log('Minus Operator');
+});
+
+inputField.addEventListener("input", () => {
+  let enteredValue = parseInt(inputField.value);
+  if (enteredValue < 1) {
+    inputField.value = 1;
+  }
+  if (enteredValue > 10) {
+    inputField.value = 1;
+
+  }
+});
+ 
+const resetCounter = () => {
+  counterValue = 0;
+  counterDisplay.innerText = counterValue;
+};
+
+resetButton.addEventListener("click", resetCounter);
+})()
